005b573fca520865bce5b4c69348f38ba07fc197
phunglongnguyen <phunglongnguyen@kpmg.com.sg>
2021-11-27 16:29:52 +0700

    Implement shop page, collection preview, collection item components, finish lession 83

---
+++ src/pages/shop/shop.component.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/preview-collection/preview-collection.component";
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherSectionsProps }) => (
+          <CollectionPreview key={id} {...otherSectionsProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
